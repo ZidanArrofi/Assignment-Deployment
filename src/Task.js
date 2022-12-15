734f0cd795186f2d4d917de134530beca03cfa1b
Zidan Arrofi <zidan.arrofi64@gmail.com>
2022-12-15 15:50:47 +0700

    Add File to Github

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Zidan Arrofi" // TODO: replace this
+const studentId = "FE2775760" // TODO: replace this
+
+export { url, studentName, studentId }
